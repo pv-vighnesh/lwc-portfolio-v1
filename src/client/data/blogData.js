50e2a5967af3cf8f4714cbd658060288f4535482
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-06-20 23:10:01 +0530

    Blogs component

---
+++ src/client/data/blogData.js
@@ -0,0 +1,30 @@
+const BLOG_DATA = [
+    {
+        title: 'Lightning Web Components - Introduction',
+        description: 'A Quick intro to LWC',
+        link: 'https://dev.to/pvvighnesh/lightning-web-components-introduction-1o0p'
+    },
+    {
+        title: 'Lightning Web Components - Adding Styles and JavaScript',
+        description: 'Learn how to style your component',
+        link: 'https://dev.to/pvvighnesh/lightning-web-components-adding-styles-and-javascript-4h2f'
+    },
+    {
+        title: 'Lightning web components - More JavaScript!',
+        description: 'Playing around with JavaScript and LWC',
+        link: 'https://dev.to/pvvighnesh/lightning-web-components-more-javascript-24bn'
+    },
+    {
+        title: 'Lightning web components - Conditional Rendering',
+        description: 'Learn how to render components conditionally',
+        link: 'https://dev.to/pvvighnesh/lightning-web-components-conditional-rendering-728'
+    },
+    {
+        title: 'Lightning web components - Rendering Lists',
+        description: 'Learn how to render lists',
+        link: 'https://dev.to/pvvighnesh/lightning-web-components-rendering-lists-1ge7'
+    }
+];
+
+export default BLOG_DATA;
+
---
+++ src/client/modules/my/blog/blog.js
@@ -0,0 +1,6 @@
+import { LightningElement } from "lwc";
+import BLOG_DATA from '../../../data/blogData';
+
+export default class Blog extends LightningElement {
+    blogData = BLOG_DATA;
+}
