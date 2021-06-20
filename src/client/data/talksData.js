0dbf64c5cea104923cdb99f44ede3da7bedf9c6a
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-06-21 00:01:17 +0530

    Talks section, added image for talks

---
+++ src/client/data/talksData.js
@@ -0,0 +1,12 @@
+const TALK_DATA = [
+    {
+        image: '../resources/images/talk-one.jpg',
+        title: 'Salesforce Job-Ready launch',
+    },
+    {
+        image: '../resources/images/talk-two.png',
+        title: 'Non-IT to Salesforce as a Career',
+    } 
+];
+
+export default TALK_DATA;
---
+++ src/client/modules/my/talks/talks.js
@@ -0,0 +1,8 @@
+import { LightningElement } from "lwc";
+import TALK_DATA from '../../../data/talksData';
+
+export default class Talks extends LightningElement {
+
+    talksData = TALK_DATA;
+
+}
