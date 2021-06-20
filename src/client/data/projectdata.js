c562ef39463b8a8e8ab1a7903e16745a3e9ef3cd
PV Vighnesh <pvighnesh.vishnu@gmail.com>
2021-06-20 23:32:28 +0530

    Adding Project section

---
+++ src/client/data/projectdata.js
@@ -0,0 +1,40 @@
+const PROJECT_DATA = [
+    {
+        title: 'lwc-portfolio',
+        description: 'A developer portfolio build using LWC OSS',
+        link: 'https://github.com/pv-vighnesh/lwc-portfolio-v1'
+    },
+    {
+        title: 'lwc-resume',
+        description: 'Resume build using LWC and Salesforce Experience Cloud (WIP)',
+        link: 'https://github.com/pv-vighnesh/lwc-resume'
+    },
+    {
+        title: 'responsive-nav',
+        description: 'A Responsive Navbar design',
+        link: 'https://github.com/pv-vighnesh/responsive-nav'
+    },
+    {
+        title: 'personal-expense-app',
+        description: 'Personal Expense application built using VanillaJs',
+        link: 'https://github.com/pv-vighnesh/personal-expense-app'
+    },
+    {
+        title: 'website-mockups',
+        description: 'Repo containing different website mockups',
+        link: 'https://github.com/pv-vighnesh/wesite-mockups'
+    },
+    {
+        title: 'CLI-app',
+        description: 'A CLI app built using NodeJs',
+        link: 'https://github.com/pv-vighnesh/CLI-App'
+    },
+    {
+        title: 'JavaScript30',
+        description: 'Collection of Simple JavaScript projects',
+        link: 'https://github.com/pv-vighnesh/JavaScript30'
+    }
+];
+
+export default PROJECT_DATA;
+
--- src/client/modules/my/projects/projects.js
+++ src/client/modules/my/projects/projects.js
@@ -1,4 +1,8 @@
 import { LightningElement } from "lwc";
+import PROJECT_DATA from '../../../data/projectdata';
 
 export default class Projects extends LightningElement {
+
+    projectData = PROJECT_DATA;
+
 }
